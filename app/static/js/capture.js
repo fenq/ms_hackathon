44e13cf9582ff4a84c4dade02fcbe9e5f5f8cc10
jnijeremy <nizeyu123@gmail.com>
2018-02-24 12:48:34 -0800

    Hook up cam and face page together.

--- app/static/js/capture.js
+++ app/static/js/capture.js
@@ -87,7 +87,7 @@
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     var data = canvas.toDataURL('image/png');
-    photo.setAttribute('src', data);
+    // photo.setAttribute('src', data);
 
   }
 
@@ -96,6 +96,102 @@
   // format data URL. By drawing it on an offscreen canvas and then
   // drawing that to the screen, we can change its size and/or apply
   // other changes before drawing it.
+
+  function makeblob(dataURL) {
+      var BASE64_MARKER = ';base64,';
+      if (dataURL && dataURL.indexOf(BASE64_MARKER) == -1) {
+          var parts = dataURL.split(',');
+          var contentType = parts[0].split(':')[1];
+          var raw = decodeURIComponent(parts[1]);
+          return new Blob([raw], { type: contentType });
+      }
+      var parts = dataURL.split(BASE64_MARKER);
+      var contentType = parts[0].split(':')[1];
+      var raw = window.atob(parts[1]);
+      var rawLength = raw.length;
+
+      var uInt8Array = new Uint8Array(rawLength);
+
+      for (var i = 0; i < rawLength; ++i) {
+          uInt8Array[i] = raw.charCodeAt(i);
+      }
+
+      return new Blob([uInt8Array], { type: contentType });
+  }
+
+  function processImage(sourceImageUrl) {
+        // **********************************************
+        // *** Update or verify the following values. ***
+        // **********************************************
+
+        // Replace the subscriptionKey string value with your valid subscription key.
+        var subscriptionKey = "0fd94478cbeb4d77ae45fac37bda9386";
+
+        // Replace or verify the region.
+        //
+        // You must use the same region in your REST API call as you used to obtain your subscription keys.
+        // For example, if you obtained your subscription keys from the westus region, replace
+        // "westcentralus" in the URI below with "westus".
+        //
+        // NOTE: Free trial subscription keys are generated in the westcentralus region, so if you are using
+        // a free trial subscription key, you should not need to change this region.
+        var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect";
+
+        // Request parameters.
+        var params = {
+            "returnFaceId": "true",
+            "returnFaceLandmarks": "false",
+            "returnFaceAttributes": "age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise",
+        };
+
+        // Display the image.
+        // var sourceImageUrl = document.getElementById("inputImage").value;
+        document.querySelector("#sourceImage").src = sourceImageUrl;
+
+        // Perform the REST API call.
+        // $.ajax({
+        //     url: uriBase + "?" + $.param(params),
+
+        //     // Request headers.
+        //     beforeSend: function(xhrObj){
+        //         xhrObj.setRequestHeader("Content-Type","application/json");
+        //         xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        //     },
+
+        //     type: "POST",
+
+        //     // Request body.
+        //     data: '{"url": ' + '"' + sourceImageUrl + '"}',
+        // })
+        $.ajax({
+            url: uriBase + "?" + $.param(params),
+
+            // Request headers.
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/octet-stream");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+
+            type: "POST",
+            processData: false,
+            // Request body.
+            data: makeblob(sourceImageUrl),
+        })
+
+        .done(function(data) {
+            // Show formatted JSON on webpage.
+            var emotions = data[0].faceAttributes.emotion;
+            $("#responseTextArea").val(JSON.stringify(emotions));
+        })
+
+        .fail(function(jqXHR, textStatus, errorThrown) {
+            // Display error message.
+            var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+            errorString += (jqXHR.responseText === "") ? "" : (jQuery.parseJSON(jqXHR.responseText).message) ? 
+                jQuery.parseJSON(jqXHR.responseText).message : jQuery.parseJSON(jqXHR.responseText).error.message;
+            alert(errorString);
+        });
+    };
 
   function takepicture() {
     var context = canvas.getContext('2d');
@@ -106,9 +202,10 @@
 
       var data = canvas.toDataURL('image/png');
       console.log(data)
-      photo.setAttribute('src', data);
+      // photo.setAttribute('src', data);
       photo1.setAttribute('value', data);
 
+      processImage(data);
     } else {
       clearphoto();
     }
